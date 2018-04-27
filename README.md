c697ee06b3ad036cf5dd4fe3da5ba78016619c63
Kino  Rose <kinoarose@gmail.com>
2018-04-27 16:58:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# PDF Extractor
+
+
+# How to Run
+```
+npm install
+
+node index.js fileName
+```
+
+## Binary Dependencies
+
+* pdfk
+* pdftotext
+* ghostscript
+* tesseract
---
+++ index.js
@@ -0,0 +1,17 @@
+const extract = require("pdf-text-extract");
+const path = require("path");
+const fs = require("fs");
+const args = process.argv.slice(2);
+
+const stringListToText = (list) => list.reduce((initial, curr) =>  initial += curr + "\n", "");
+
+extract(path.resolve(args[0]),{splitPages: true}, (err, text) => {
+  if(err) {
+    console.error(err);
+    return;
+  } else {
+    const data = stringListToText(text);
+    fs.writeFileSync("output.txt",data,"utf8");
+  }
+})
+
